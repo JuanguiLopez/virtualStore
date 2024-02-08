7e46d4de51364fabd0506b1c8abe4cdfd4a6415f
Juan Guillermo Lopez <glopez@veritran.com>
2024-02-07 22:50:06 -0500

    Segunda entrega del desafio de sockets y handlebars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const { Server } = require("socket.io");
+const ProductManager = require("./ProductManager");
 
 const productsRouter = require("./routes/products.router");
 const cartsRouter = require("./routes/carts.router");
 const viewsRouter = require("./routes/views.router");
+
+const prodManager = new ProductManager(__dirname + "/files/ProductsJG.json");
 
 const port = 8080;
 
@@ -21,6 +24,7 @@ app.engine("handlebars", handlebars.engine());
 app.set("views", `${__dirname}/views`);
 app.set("view engine", "handlebars");
 
+// asigna endpoints a routers
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
@@ -33,14 +37,24 @@ const server = app.listen(port, () =>
 const io = new Server(server);
 
 io.on("connection", (socket) => {
-  console.log("connected " + socket.id);
+  console.log(`socket ${socket.id} conectado`);
 
   socket.on("disconnect", () => {
-    console.log(`socket ${socket.id} disconnected`);
+    console.log(`socket ${socket.id} desconectado`);
   });
 
-  socket.on("message", (message) => {
-    console.log(message);
-    socket.emit("answer", "Hola, este es un mensaje desde el servidor");
+  socket.on("crear producto", async (newProduct) => {
+    console.log(newProduct);
+    await prodManager.addProduct(newProduct);
+    const products = await prodManager.getProducts();
+
+    io.emit("actualizar lista", { products });
+  });
+
+  socket.on("eliminar producto", async ({ id }) => {
+    await prodManager.deleteProduct(id);
+    const products = await prodManager.getProducts();
+
+    io.emit("actualizar lista", { products });
   });
 });
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,15 +1,56 @@
 const socket = io();
 
-const realTimeTable = document.getElementById("messages");
+const iptTitulo = document.getElementById("iptTitulo");
+const iptDescripcion = document.getElementById("iptDescripcion");
+const iptCodigo = document.getElementById("iptCodigo");
+const iptStock = document.getElementById("iptStock");
+const iptPrecio = document.getElementById("iptPrecio");
+const iptCategoria = document.getElementById("iptCategoria");
+const fiThumbnails = document.getElementById("fiThumbnails");
 const btnAgregar = document.getElementById("btnAgregar");
+
+const realTimeTable = document.getElementById("realTimeTable");
+const btnEliminar = document.getElementById("btnEliminar");
 
 //socket.emit("message", "Hello server, sending message from a websocket");
 
 btnAgregar.addEventListener("click", () => {
-  socket.emit("message", "Hello server, a product was created successfully");
+  socket.emit("crear producto", {
+    title: iptTitulo.value,
+    description: iptDescripcion.value,
+    code: iptCodigo.value,
+    price: iptPrecio.value,
+    stock: iptStock.value,
+    category: iptCategoria.value,
+    //thumbnails: fiThumbnails.value,
+  });
+
+  iptTitulo.value = "";
+  iptDescripcion.value = "";
+  iptCodigo.value = "";
+  iptPrecio.value = "";
+  iptStock.value = "";
+  iptCategoria.value = "";
+  //fiThumbnails.value = "";
 });
 
-socket.on("answer", (message) => {
-  realTimeTable.innerHTML += `<br />${message}`;
-  //console.log(`message from server: ${message}`);
+// Eventos server
+socket.on("actualizar lista", ({ products }) => {
+  realTimeTable.innerHTML = "";
+  products.forEach((prod) => {
+    realTimeTable.innerHTML += `
+      <tr>
+        <td>${prod.id}</td>
+        <td>${prod.title}</td>
+        <td>${prod.description}</td>
+        <td>${prod.price}</td>
+        <td>${prod.stock}</td>
+        <td>${prod.category}</td>
+        <td><button onclick="eliminarProducto(${prod.id})">Eliminar</button></td>
+      </tr>`;
+  });
 });
+
+function eliminarProducto(id) {
+  socket.emit("eliminar producto", { id });
+}
